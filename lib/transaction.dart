951cf65513ef0f08af90948bed340e04971b0852
kenLi <eu31@18m.dev>
2021-03-11 18:34:42 +0800

    1.remove not use code

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,6 +1,3 @@
-import 'package:apihandler/apiHandler.dart';
-import 'package:transaction/model/coinPrice.dart';
-
 class Transaction {
   static final Transaction _instance = Transaction._internal();
 
@@ -9,15 +6,6 @@ class Transaction {
 
   factory Transaction() {
     return _instance;
-  }
-
-  /// getTopErc20CoinPrice
-  Future<CoinPriceList> getTopErc20CoinPrice() async {
-    var result = await apiHandler.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=wrapped-bitcoin%2Cchainlink%2Cuniswap%2Cyearn-finance%2Cokb%2Comisego%2Cmaker%2Cbasic-attention-token%2Cftx-token%2Ccompound-ether&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-    // Map<String, dynamic> resultData = jsonDecode(result);
-    CoinPriceList coinPrice = CoinPriceList.fromJson(result);
-    return coinPrice;
   }
 }
 
